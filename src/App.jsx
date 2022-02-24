e7143bf884464a9f75c782530d0a44fd0cf043c3
Rupam Shil <rupamshil111@gmail.com>
2022-02-24 23:40:37 +0530

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,32 @@ import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { API_KEY } from './dotenv';
+import Movie from './Movie';
+import Filter from './Filter';
+
 function App() {
 	const [moviesArray, setMoviesArray] = useState([]);
-	const fetchPopular = async () => {
-		const res = await fetch(
-			`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-		);
-		const movies = await res.json();
-		setMoviesArray([...movies.results]);
-		console.log(moviesArray);
-	};
 
 	useEffect(() => {
 		fetchPopular();
 	}, []);
 
+	const fetchPopular = async () => {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+		);
+		const movies = await res.json();
+		setMoviesArray(movies.results);
+	};
+
 	return (
-		<div className="className">
-			<h1>Hello</h1>
+		<div className="App">
+			<Filter />
+			<div className="popular-movies">
+				{moviesArray.map((movie) => {
+					return <Movie key={movie.id} movie={movie} />;
+				})}
+			</div>
 		</div>
 	);
 }
---
+++ src/Movie.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+function Movie({ movie }) {
+	return (
+		<div>
+			<h2>{movie.title}</h2>
+			<img
+				src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+				alt=""
+			/>
+		</div>
+	);
+}
+
+export default Movie;
